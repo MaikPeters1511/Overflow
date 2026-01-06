e2f056b70b042d640be4d062c25bb2aa0bc57921
Maiki <bewerbung@maikpeters.org>
2026-01-06 14:28:01 +0100

    Integrate Keycloak as Identity Provider with admin credentials and update launch settings

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -1,3 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Keycloak hinzufügen (Identity Provider) mit Admin-Credentials
+
+var keyCloak = builder
+    .AddKeycloak("keycloak", port: 6001)
+    .WithImage("keycloak/keycloak", "26.4.7-0")
+    .WithContainerName("Keycloak-Server")
+    .WithDataBindMount("./keycloak-data")
+    .WithLifetime(ContainerLifetime.Persistent);
+
 builder.Build().Run();
